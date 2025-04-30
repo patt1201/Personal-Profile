4b109b3a503603b4de94e20f9250b33217acaeb0
patt1201 <aidallajamespatrick46@gmail.com>
2025-04-30 12:05:00 +0800

    Update education.js

--- personal-profile/src/components/education.js
+++ personal-profile/src/components/education.js
@@ -1,40 +1,16 @@
 import React from 'react';
 
-function education() {
-  const education = [
-    {
-      school: "University of Cabuyao",
-      degree: "Bachelor of Science in Information Technology",
-      year: "2018 - 2022"
-    },
-    {
-      school: "High School Name",
-      degree: "High School Diploma",
-      year: "2014 - 2018"
-    }
-  ];
-
+const education = () => {
   return (
-    <section className="education-section">
+    <div className="education">
       <h2>Education</h2>
-      <div className="education-list">
-        {education.map((edu, index) => (
-          <div className="education-item" key={index}>
-            <h3>{edu.school}</h3>
-            <p className="degree">{edu.degree}</p>
-            <p className="year">{edu.year}</p>
-            <div className="courses">
-              <h4>Key Courses:</h4>
-              <ul>
-                {edu.courses.map((course, courseIndex) => (
-                  <li key={courseIndex}>{course}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+      <ul>
+        <li>
+          <h3>University of Cabuyao</h3>
+          <p>Bachelor of Science in Information Technology</p>
+        </li>
+      </ul>
+    </div>
   );
 }
 
